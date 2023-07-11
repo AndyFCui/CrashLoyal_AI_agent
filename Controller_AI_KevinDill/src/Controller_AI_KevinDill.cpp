d3d78b30c600ce4e04d6ddb8d0d6c0652ba05f65
XIANGYU CUI <136183369@qq.com>
2023-07-11 18:03:07 -0400

    Add files via upload

---
+++ Controller_AI_KevinDill/src/Controller_AI_KevinDill.cpp
@@ -0,0 +1,51 @@
+// MIT License
+// 
+// Copyright(c) 2020 Arthur Bacon and Kevin Dill
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright  this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "Controller_AI_KevinDill.h"
+
+#include "Constants.h"
+#include "EntityStats.h"
+#include "iPlayer.h"
+#include "Vec2.h"
+
+static const Vec2 ksGiantPos(LEFT_BRIDGE_CENTER_X, RIVER_TOP_Y - 0.5f);
+static const Vec2 ksArcherPos(LEFT_BRIDGE_CENTER_X, 0.f);
+
+void Controller_AI_KevinDill::tick(float deltaTSec)
+{
+    assert(m_pPlayer);
+
+    // wait for elixir
+    if (m_pPlayer->getElixir() >= 9)
+    {
+        // convert the positions from player space to game space
+        bool isNorth = m_pPlayer->isNorth();
+        Vec2 giantPos_Game = ksGiantPos.Player2Game(isNorth);
+        Vec2 archerPos_Game = ksArcherPos.Player2Game(isNorth);
+
+        // Create two archers and a giant
+        m_pPlayer->placeMob(iEntityStats::Giant, giantPos_Game);
+        m_pPlayer->placeMob(iEntityStats::Archer, archerPos_Game);
+        m_pPlayer->placeMob(iEntityStats::Archer, archerPos_Game);
+    }
+}
+
---
+++ Controller_UI/src/Controller_UI.cpp
@@ -0,0 +1,77 @@
+// MIT License
+// 
+// Copyright(c) 2020 Arthur Bacon and Kevin Dill
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "Controller_UI.h"
+
+#include "Constants.h"
+#include "EntityStats.h"
+#include "iPlayer.h"
+#include "Vec2.h"
+
+#include "SDL.h"
+
+Controller_UI* Singleton<Controller_UI>::s_Obj = NULL;
+
+Controller_UI::~Controller_UI()
+{
+    std::cout << "Controller_UI is being deleted. This probably means that "
+        << "you made more than one." << std::endl;
+}
+
+void Controller_UI::tick(float deltaTSec) {
+    while(!events.empty()) {
+        SDL_Event e = events.front();
+        events.pop();
+
+        if ((e.type == SDL_MOUSEBUTTONUP) && (e.button.button == SDL_BUTTON_LEFT)) {
+            int pixelX = -1;
+            int pixelY = -1;
+            SDL_GetMouseState(&pixelX, &pixelY);
+            const Vec2 mousePos((float)(pixelX / PIXELS_PER_METER), (float)(pixelY / PIXELS_PER_METER));
+
+            iEntityStats::MobType mobType;
+            if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_A])
+            {
+                mobType = iEntityStats::Archer;
+            }
+            else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_S])
+            {
+                mobType = iEntityStats::Swordsman;
+            }
+            else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_G])
+            {
+                mobType = iEntityStats::Giant;
+            }
+            else
+            {
+                continue;
+            }
+
+            assert(m_pPlayer);
+            m_pPlayer->placeMob(mobType, mousePos);
+        }
+    }
+}
+
+void Controller_UI::loadEvent(SDL_Event e) {
+    events.push(e);
+}
